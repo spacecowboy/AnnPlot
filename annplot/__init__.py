deccdc55170e344d898ff7385ccde4230f3e0832
Jonas Kalderstam <jonask@thep.lu.se>
2012-05-31 16:38:44 +0200

    added feature importance to init
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- annplot/__init__.py
+++ annplot/__init__.py
@@ -1,4 +1,4 @@
 #Stuff we want available at the top-level
 from util import show, save
-from performance import plot_train_vs_test, plot_roc
+from performance import plot_train_vs_test, plot_roc, plot_feature_importance
 from classification import plot_dataset, plot_boundaries
